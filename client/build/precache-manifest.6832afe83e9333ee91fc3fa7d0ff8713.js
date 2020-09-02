551002201cdc09c6353b5c56463c2d57bb962827
shub12133 <shub.123433@gmail.com>
2020-09-02 19:43:06 +0530

    deploy

---
+++ client/build/precache-manifest.6832afe83e9333ee91fc3fa7d0ff8713.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ff84af16085f22fca1dc8e73474da3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d15f7e492a33a2144b37",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "ce7011bcc0ff68397fa3",
+    "url": "/static/css/main.959a1e2b.chunk.css"
+  },
+  {
+    "revision": "d15f7e492a33a2144b37",
+    "url": "/static/js/2.e09011a7.chunk.js"
+  },
+  {
+    "revision": "507a7370b52a72b5f39c2fecbeb4dc40",
+    "url": "/static/js/2.e09011a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce7011bcc0ff68397fa3",
+    "url": "/static/js/main.b171978a.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
